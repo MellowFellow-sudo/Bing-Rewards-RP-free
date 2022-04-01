7c05b82ec6c2853c15e56052545df2a8ac55312a
Neko <izangandiaruiz@gmail.com>
2022-04-02 01:10:36 +0200

    Migration to Edge and started working on tasks

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Bing-Rewards-RP-free
-Program that makes the daily searches for PC and Mobile for now. (150 points each day)
+Program that makes the daily searches for PC and Mobile for now.
+*The points you can win depends on the level of the account.*
 
 # How to use 📝
-First of all, you need to login to your account on Chrome. The program uses Chrome data to make the searches for avoid save credentials and make the login every time the program executes.
+First of all, you need to login to your account on Edge. The program uses the browser data to prevent the saving of credentials and avoid logging every time the program executes.
 
 ![image](https://user-images.githubusercontent.com/57575090/160806521-50ecacf6-cda4-4aaf-9a15-e9bd0f059bed.png)
 
@@ -12,13 +13,11 @@ And, finally, change the Windows User on:
 ```py
 WINDOWS_USER = "Neko" # Change this to your username
 ```
-
-Run the program and it automatically starts Chrome and makes the searches on PC mode and Mobile mode.
+Run the program and it automatically starts Edge and makes the searches on PC mode and Mobile mode.
 
 # Important! ⚠️
-You can't use Chrome when te program is running, so you need to close Chrome before running the program.
-That's why it takes the Chrome data.
-I don't know if it can be fixed :c
+You can't use Edge when te program is running, so you need to close Edge before running the program.
+That's why it takes the Edge data.
 
 # Workflow 💡
 - [ ] Do daily tasks
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from subprocess import CompletedProcess
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
 from colorama import Fore, init
 from prettytable import PrettyTable
-import requests, random, math
+import requests, random, math, time
 init()
 
 WINDOWS_USER = "Neko" # Change this to your username
@@ -66,40 +67,34 @@ def art_mobile():
     ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚══════╝╚══════╝
                                                 
     """)
-    
-def art_edge():
-    print("""
-        
-    ███████╗██████╗  ██████╗ ███████╗
-    ██╔════╝██╔══██╗██╔════╝ ██╔════╝
-    █████╗  ██║  ██║██║  ███╗█████╗  
-    ██╔══╝  ██║  ██║██║   ██║██╔══╝  
-    ███████╗██████╔╝╚██████╔╝███████╗
-    ╚══════╝╚═════╝  ╚═════╝ ╚══════╝
-                                    
-    """)
 
-def pcDriver():
-    s = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    options.add_argument(f'--user-data-dir=C:/Users/{WINDOWS_USER}/AppData/Local/Google/Chrome/User Data/')
+def pcDriver(driver):
+    try: driver.quit()
+    except: pass
+
+    s = Service(EdgeChromiumDriverManager().install())
+    options = webdriver.EdgeOptions()
+    options.add_argument(f"user-data-dir=C:\\Users\\{WINDOWS_USER}\\AppData\\Local\\Microsoft\\Edge\\User Data")
     options.add_argument("--log-level=3")
-    try: driver = webdriver.Chrome(service=s, options=options)
-    except Exception as error: return f"\n{RED}[{WHITE}error{RED}] Close Chrome before using this program :3{RESET}\n{error}"
+    try: driver = webdriver.Edge(service=s, options=options)
+    except Exception as error: return f"\n{RED}[{WHITE}error{RED}] Close Edge before using this program :3{RESET}\n{error}"
 
     return driver
 
-def mobileDriver():
-    s = Service(ChromeDriverManager().install())
-    mobile_emulation = {
-    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+def mobileDriver(driver):
+    try: driver.quit()
+    except: pass
 
-    options = webdriver.ChromeOptions()
+    s = Service(EdgeChromiumDriverManager().install())
+    mobile_emulation = {
+    "userAgent": "Mozilla/5.0 (Linux; Android 5.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+
+    options = webdriver.EdgeOptions()
     options.add_experimental_option("mobileEmulation", mobile_emulation)
-    options.add_argument(f'--user-data-dir=C:/Users/{WINDOWS_USER}/AppData/Local/Google/Chrome/User Data/')
+    options.add_argument(f"user-data-dir=C:\\Users\\{WINDOWS_USER}\\AppData\\Local\\Microsoft\\Edge\\User Data")
     options.add_argument("--log-level=3")
     try: driver = webdriver.Chrome(service=s, options=options)
-    except Exception as error: return f"\n{RED}[{WHITE}error{RED}] Close Chrome before using this program :3{RESET}\n{error}"
+    except Exception as error: return f"\n{RED}[{WHITE}error{RED}] Close Edge before using this program :3{RESET}\n{error}"
     
     return driver
 
@@ -108,10 +103,20 @@ def search(driver, total):
         word = new_search()
         print(f"{CYAN}[{WHITE}{i+1}{CYAN}] Search: {BLUE}{word}{RESET}")
         driver.get(f"https://www.bing.com/search?q={word}&qs=n&form=QBRE&sp=-1&pq=aaaa&sc=8-4&sk=&cvid=68BA88FDD17C49629D9563F0C2E1FEF1")
-    driver.quit()
 
-def edge(driver, total):
-    None 
+def tasks(driver):
+    driver.get("https://rewards.bing.com")
+
+    time.sleep(2)
+    t1 = driver.find_element(By.XPATH, '//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[1]')
+    t1.click()
+
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="id_rh"]').click()
+
+    t2 = driver.find_element(By.XPATH, '//*[@id="modern-flyout"]/div/div[3]/div[2]/div[1]/div[2]/div/div[2]').click()
+    t3 = driver.find_element(By.XPATH, '//*[@id="modern-flyout"]/div/div[3]/div[2]/div[1]/div[2]/div/div[3]').click()
+    input()
 
 def getStatus(driver):
     global lvl_1
@@ -120,18 +125,18 @@ def getStatus(driver):
     table = PrettyTable([f'{WHITE}Task type{RESET}', f'{WHITE}Remaining points{RESET}', f'{WHITE}Searches{RESET}'])
     while True:
         try:
-            pc = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-            total_pc = 90 - int(pc)
+            pc = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text.split(' / ')
+            total_pc = int(pc[1]) - int(pc[0])
             table.add_row([f'{GREEN if total_pc == 0 else RED}PC{RESET}', f'{GREEN if total_pc == 0 else RED}{total_pc}{RESET}', f'{GREEN if total_pc == 0 else RED}{math.ceil(total_pc/3)}{RESET}'])
 
-            mobile = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-            total_mobile = 60 - int(mobile)
+            mobile = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text.split(' / ')
+            total_mobile = int(mobile[1]) - int(mobile[0])
             table.add_row([f'{GREEN if total_mobile == 0 else RED}Mobile{RESET}', f'{GREEN if total_mobile == 0 else RED}{total_mobile}{RESET}', f'{GREEN if total_mobile == 0 else RED}{math.ceil(total_mobile/3)}{RESET}'])
 
             try:
-                edge = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[3]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-                total_edge = 12 - int(edge)
-                table.add_row([f'{GREEN if total_edge == 0 else RED}Edge{RESET}', f'{GREEN if total_edge == 0 else RED}{total_edge}{RESET}','-'])
+                edge = driver.find_element(By.XPATH, '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[3]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text.split(' / ')
+                total_edge = int(edge[1]) - int(edge[0])
+                table.add_row([f'{GREEN if total_edge == 0 else RED}Edge{RESET}', f'{GREEN if total_edge == 0 else RED}{total_edge}{RESET}',f'{GREEN if total_edge == 0 else RED}{math.ceil(total_edge/3)}{RESET}'])
             except:
                 lvl_1 = True
                 total_edge = 0
@@ -143,12 +148,13 @@ def getStatus(driver):
 def main():
     global lvl_1
     check = True
+    driver = None
 
     while check:
         check = False
 
         # Get information of the remaining tasks
-        driver = pcDriver()
+        driver = pcDriver(driver)
         if "error" in str(driver):
             print(driver)
             return
@@ -160,18 +166,23 @@ def main():
             search(driver, data[0])
             check = True
 
+        if data[2] > 0 and data[0] == 0:
+            art_pc()
+            search(driver, data[2])
+            check = True
+
         # Complete the Mobile searches
         if data[1] > 0 and not lvl_1:
             art_mobile()
-            driver = mobileDriver()
+            driver = mobileDriver(driver)
             if "error" in str(driver):
                 print(driver)
                 return
             search(driver, data[1])
             check = True
 
-        # Probably complete the Edge task in future c:
-
+    open('usedKeywords.txt', 'w').write("")
+    # tasks(driver)
     print(f"\n{GREEN}[{WHITE}·{GREEN}] Done!{RESET}") # Add total poinst gotten c:
 
 if __name__ == "__main__":
