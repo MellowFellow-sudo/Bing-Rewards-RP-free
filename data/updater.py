6c750b8b8702619f67ff84f90b446d808d500519
Neko <izangandiaruiz@gmail.com>
2022-07-24 13:29:53 +0200

    UwU another time

--- data/updater.py
+++ data/updater.py
@@ -8,14 +8,13 @@ def run(cmd):
 
 def git_update():
     process = run("git reset --hard HEAD; git clean -d -f; git pull")
-    print(str(process))
+    # print(str(process))
 
     if 'changed' in str(process): print(Fore.LIGHTGREEN_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTGREEN_EX}] Updated" + Fore.RESET)
     else: print(Fore.LIGHTRED_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTRED_EX}] Not a git repository" + Fore.RESET)
 
 def git_check():
     process = run("git pull")
-    print(f"{process.stdout} - {process.stderr}")
 
     if "Already up to date" in str(process.stdout): 
         print(Fore.LIGHTGREEN_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTGREEN_EX}] Already up to date" + Fore.RESET)
